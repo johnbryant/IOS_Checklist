9e8f0373694642aeb51db712c790c54380db53f7
johnbryant <johnbryant262@gmail.com>
2016-03-15 14:10:03 +0800

    Multiple Checklist - 1

---
+++ Checklists/ListDetailViewController.swift.swift
@@ -0,0 +1,70 @@
+//
+//  ListDetailViewController.swift.swift
+//  Checklists
+//
+//  Created by JohnBryant on 16/3/15.
+//  Copyright © 2016年 JohnBryant. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol ListDetailViewControllerDelegate: class {
+    func listDetailViewControllerDidCancel(controller: ListDetailViewController)
+    func listDetailViewController(controller: ListDetailViewController, didFinishAddingChecklist checklist: Checklist)
+    func listDetailViewController(controller: ListDetailViewController, didFinishEditingChecklist checklist: Checklist)
+}
+
+
+class ListDetailViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    weak var delegate: ListDetailViewControllerDelegate?
+    var checklistToEdit: Checklist?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let checklist = checklistToEdit {
+            title = "Edit Checklist"
+            textField.text = checklist.name
+            doneBarButton.enabled = true
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    
+    @IBAction func cancel() {
+        delegate?.listDetailViewControllerDidCancel(self)
+    }
+    
+    
+    @IBAction func done() {
+        if let checklist = checklistToEdit {
+            delegate?.listDetailViewController(self, didFinishEditingChecklist: checklist)
+        } else {
+            let checklist = Checklist(name: textField.text!)
+            delegate?.listDetailViewController(self, didFinishAddingChecklist: checklist)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        return nil
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let oldString: NSString = textField.text!
+        let newString: NSString = oldString.stringByReplacingCharactersInRange(range, withString: string)
+        doneBarButton.enabled = (newString.length > 0)
+        return true
+    }
+    
+    
+}
